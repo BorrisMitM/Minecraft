e8178c8bf43fc17977b10f8a92fe21a5a67ab07e
Morris <mauriceandreas@aol.com>
2021-04-20 12:29:09 +0200

    asd

--- Chunk.cpp
+++ Chunk.cpp
@@ -9,27 +9,32 @@ void Chunk::SetVisibility()
 		{
 			for (int z = 0; z < 16; z++)
 			{
-  				if (cubes[x][y][z] == NULL) continue; //extend this only activate the sides that need to, maybe a bool array or sth 
+  				if (cubes[x][y][z] == NULL) continue; 
 				//this can be used later for destroying blocks and activating neighboring blocks
-				if (x <= 0)	 continue;
-				if (y <= 0)	 continue;
-				if (z <= 0)	 continue;
-				if (x >= 15)	 continue;
-				if (y >= 255) continue;
-				if (z >= 15)	 continue;
-				cubes[x][y][z]->SetVisibilty(3, cubes[x - 1][y][z] == NULL);
-				cubes[x][y][z]->SetVisibilty(5, cubes[x][y - 1][z] == NULL);
-				cubes[x][y][z]->SetVisibilty(2, cubes[x][y][z - 1] == NULL);
-				cubes[x][y][z]->SetVisibilty(1, cubes[x + 1][y][z] == NULL);
-				cubes[x][y][z]->SetVisibilty(4, cubes[x][y + 1][z] == NULL);
-				cubes[x][y][z]->SetVisibilty(0, cubes[x][y][z + 1] == NULL);
-				//if (cubes[x - 1][y][z] == NULL) continue;
-				//if (cubes[x][y - 1][z] == NULL) continue;
-				//if (cubes[x][y][z - 1] == NULL) continue;
-				//if (cubes[x + 1][y][z] == NULL) continue;
-				//if (cubes[x][y + 1][z] == NULL) continue;
-				//if (cubes[x][y][z + 1] == NULL) continue;
-				//cubes[x][y][z]->visible = false;
+				//if (x <= 0)	 continue;
+				//if (y <= 0)	 continue;
+				//if (z <= 0)	 continue;
+				//if (x >= 15)	 continue;
+				//if (y >= 255) continue;
+				//if (z >= 15)	 continue;
+				if(x > 0)
+					cubes[x][y][z]->SetVisibilty(3, cubes[x - 1][y][z] == NULL);
+				else cubes[x][y][z]->SetVisibilty(3, true);
+				if(y > 0)
+					cubes[x][y][z]->SetVisibilty(5, cubes[x][y - 1][z] == NULL);
+				else cubes[x][y][z]->SetVisibilty(5, true);
+				if(z > 0)
+					cubes[x][y][z]->SetVisibilty(2, cubes[x][y][z - 1] == NULL);
+				else cubes[x][y][z]->SetVisibilty(2, true);
+				if(x < 15)
+					cubes[x][y][z]->SetVisibilty(1, cubes[x + 1][y][z] == NULL);
+				else cubes[x][y][z]->SetVisibilty(1, true);
+				if(y < 255)
+					cubes[x][y][z]->SetVisibilty(4, cubes[x][y + 1][z] == NULL);
+				else cubes[x][y][z]->SetVisibilty(4, true);
+				if(z < 15)
+					cubes[x][y][z]->SetVisibilty(0, cubes[x][y][z + 1] == NULL);
+				else cubes[x][y][z]->SetVisibilty(0, true);
 			}
 		}
 	}
